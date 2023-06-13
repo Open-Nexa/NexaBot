14d23caeb99cc2214b22eba58528848c75c1e9c0
DemuraAIdev <cyberaioff@gmail.com>
2023-06-13 17:09:07 +0700

    Verify CMD Updated

--- src/event/interactionCreate.js
+++ src/event/interactionCreate.js
@@ -6,11 +6,6 @@ module.exports = {
 		if (interaction.isChatInputCommand()) {
 			const command = interaction.client.commands.get(interaction.commandName);
 
-			if (!command) {
-				console.error(`No command matching ${interaction.commandName} was found.`);
-				return;
-			}
-
 			try {
 				await command.execute(interaction);
 			} catch (error) {
