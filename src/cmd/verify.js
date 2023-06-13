0b727b1171dac378ced7bfbaf9c65477d974b334
DemuraAIdev <cyberaioff@gmail.com>
2023-06-13 17:09:11 +0700

    modified:   src/cmd/verify.js

--- src/cmd/verify.js
+++ src/cmd/verify.js
@@ -1,20 +1,22 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { roles } = require('../config.json');
 const { EmbedBuilder } = require('discord.js');
 
 const verfyembed = new EmbedBuilder()
     .setTitle('Done')
     .setDescription('You have been verified')
     .setColor('1E4B9C')
-    .setFooter({ text: 'OPEN NEXA'})
+    .setFooter({ text: 'OPEN NEXA' })
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('verify')
-		.setDescription('Add role member'),
-        
-	async execute(interaction) {
-            await interaction.member.roles.add(roles.member);
-            await interaction.reply({ embeds: [verfyembed], ephemeral: true });
-	},
+    data: new SlashCommandBuilder()
+        .setName('verify')
+        .setDescription('Add role member'),
+
+    async execute(interaction) {
+        let roleMember = interaction.guild.roles.cache.find(
+            r => r.id === "901041302601367583"
+        );
+        await interaction.member.roles.add(roleMember);
+        await interaction.reply({ embeds: [verfyembed], ephemeral: true });
+    },
 };
